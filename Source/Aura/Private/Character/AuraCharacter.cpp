3176c6a38efb5d9ca829223b72affd9c40597d68
Lu0y3 <sakuralu0y3@gmail.com>
2024-12-15 00:08:47 +0800

    HighlightActor

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,17 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true; //人物朝向移动方向
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	//平面约束 功能，用于限制角色的移动，使其只能在一个指定平面上运动。
+	GetCharacterMovement()->bConstrainToPlane = true; //启用平面约束
+	GetCharacterMovement()->bSnapToPlaneAtStart = true; //开始时对齐平面
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// YanWei.
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
