b85033c38b52f9637f6bf433cb00744af4debd0e
Lu0y3 <sakuralu0y3@gmail.com>
2024-12-15 17:32:31 +0800

    FinishHighlight

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -4,3 +4,4 @@
 
 #include "CoreMinimal.h"
 
+#define CUSTOM_DEPTH_RED 250  //用于显示高亮的 PP_Highlight材质
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -9,6 +9,7 @@
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
+class IEnemyInterface;
 /**
  * 
  */
@@ -18,9 +19,12 @@ class AURA_API AAuraPlayerController : public APlayerController
 	GENERATED_BODY()
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
+	
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
+
 private:
 	UPROPERTY(EditAnywhere, Category="Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
@@ -29,4 +33,12 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const FInputActionValue& InputActionValue);
+
+	//玩家控制器时刻都在进行的光标交互天生的
+	void CursorTrace(); //追踪光标位置
+	IEnemyInterface* LastActor; //上一帧拾取的接口指针
+	IEnemyInterface* CurrentActor; //这一帧拾取的接口指针
+	
 };
+
+
